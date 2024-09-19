6551f9f748e5c9d43b7abec128e2d0cd712305db
nikolas.rieger <nikolas.rieger@student.hpi.de>
2024-09-19 12:40:14 +0200

    chore: split in different files

---
+++ assistant/src/engine/audio_engine/stream_controller.py
@@ -0,0 +1,83 @@
+from numpy import (
+    zeros,
+    int32,
+    frombuffer,
+    median,
+    array,
+    concatenate,
+)
+from pyaudio import paInt16, PyAudio
+from PyQt6.QtWidgets import QWidget
+from PyQt6.QtCore import QThread, QEventLoop, QTimer, pyqtSignal
+
+
+class MicThread(QThread):
+    sig = pyqtSignal(bytes)
+
+    def __init__(self, sc):
+        super(MicThread, self).__init__()
+        self.sc = sc
+        self.sig.connect(self.sc.append)
+        self.running = True
+
+    def run(self):
+        while self.running:
+            data = self.sc.stream.read(self.sc.CHUNK)
+            self.sig.emit(data)
+
+    def stop(self):
+        self.running = False
+
+
+class StreamController(QWidget):
+    def __init__(self):
+        super(StreamController, self).__init__()
+        self.data = zeros((100000), dtype=int32)
+        self.median_data = []
+        self.buffer = array([], dtype=int32)
+        self.CHUNK = 1024
+        self.CHANNELS = 1
+        self.RATE = 44100
+        self.FORMAT = paInt16
+        self.interval_ms = 150
+        self.samples_per_interval = int(self.RATE * (self.interval_ms / 1000))
+
+    def setup_stream(self):
+        self.audio = PyAudio()
+        self.stream = self.audio.open(
+            format=self.FORMAT,
+            channels=self.CHANNELS,
+            rate=self.RATE,
+            input=True,
+            frames_per_buffer=self.CHUNK,
+        )
+        self.micthread = MicThread(self)
+        self.micthread.start()
+
+    def append(self, vals):
+        vals = frombuffer(vals, "int16")
+        c = self.CHUNK
+        self.data[:-c] = self.data[c:]
+        self.data[-c:] = vals
+
+        self.buffer = concatenate([self.buffer, vals])
+
+        while len(self.buffer) >= self.samples_per_interval:
+            interval_data = self.buffer[: self.samples_per_interval]
+            median_value = median(interval_data)
+            self.median_data.append(median_value)
+            self.buffer = self.buffer[self.samples_per_interval :]
+
+        if len(self.median_data) * self.samples_per_interval > len(self.data):
+            self.median_data = self.median_data[
+                -len(self.data) // self.samples_per_interval :
+            ]
+
+    def breakdown_stream(self):
+        self.micthread.terminate()
+        self.stream.stop_stream()
+        self.stream.close()
+        self.audio.terminate()
+        loop = QEventLoop()
+        QTimer.singleShot(400, loop.quit)
+        loop.exec()
